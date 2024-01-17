5f4e27636aba59e640cb8e303d6457cd064aefc7
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2024-01-17 21:29:57 +0100

    created routes for country service

--- country-service/index.js
+++ country-service/index.js
@@ -1,12 +1,20 @@
 const express = require('express'); //creating a express server
 const app = express(); //creating an instance of express app
+const routes = require('./routes/routes');
+const mongoose = require('mongoose');
+
+app.use(routes);
 
 const port = "5000"; //port on which app will handle all requests
 
-app.get('/', (req,res) => {
-    res.send('hello world');
-})
+mongoose.connect('mongodb+srv://admin:aLDQhMWr2AQ3bsBe@cluster0.yz76vjj.mongodb.net/').
+    then(() => {
+        app.listen(port, () => {
+            console.log(`App running on ${port}`);
+        })
+        console.log('Connected to MongoDB')
+    }).catch((error) => {
+        console.log(error);
+    })
 
-app.listen(port, () => {
-    console.log(`App running on ${port}`);
-})
+
