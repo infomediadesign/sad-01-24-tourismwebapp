3d0b519436a09e7eaed5d53b7e18256375e2aa63
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2024-01-17 21:52:11 +0100

    created schema for my countryModel in DB

---
+++ country-service/models/countryModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const countrySchema = mongoose.Schema({
+    name: String,
+    imageMain: String,
+    image1: String,
+    image2: String,
+    image3: String,
+    description: String,
+})
+
+const Country = mongoose.model('Country', countrySchema);
