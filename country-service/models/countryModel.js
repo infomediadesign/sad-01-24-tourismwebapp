6467e6cd40093acbb18c5a697ab5596660dd1788
mishrarpita321 <45449505+mishrarpita321@users.noreply.github.com>
2024-01-18 00:01:14 +0100

    delivering country service changes

--- country-service/models/countryModel.js
+++ country-service/models/countryModel.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const countrySchema = mongoose.Schema({
     name: String,
-    imageMain: String,
-    image1: String,
-    image2: String,
-    image3: String,
+    imageMain: Buffer,
+    image1: Buffer,
+    image2: Buffer,
+    image3: Buffer,
     description: String,
 })
 
 const Country = mongoose.model('Country', countrySchema);
+module.exports = Country;
