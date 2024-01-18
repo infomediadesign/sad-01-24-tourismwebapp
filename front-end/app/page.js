c80624f872518437caaebd60db3f27e1518b5920
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-01-18 23:35:15 +0100

    Your commit message

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -7,19 +7,24 @@
 
   export default function Home() {
     return (
-      <div>
+      <><div>
         <Navbar />
-  
-       {/*<div className={styles.imageContainer} style={{ backgroundImage: `url('/Images/Pic.jpeg')` }}>*/}
-       <div className={styles.imagecontainer}>
-      <div className={`${styles.image} ${styles.leftImage}`}></div>
-      <div className={`${styles.image} ${styles.centerImage}`}></div>
-      <div className={`${styles.image} ${styles.rightImage}`}></div>
+
+        {/*<div className={styles.imageContainer} style={{ backgroundImage: `url('/Images/Pic.jpeg')` }}>*/}
+        <div className={styles.imagecontainer}>
+          <div className={`${styles.image} ${styles.leftImage}`}></div>
+          <div className={`${styles.image} ${styles.centerImage}`}></div>
+          <div className={`${styles.image} ${styles.rightImage}`}></div>
         </div>
-          <div className={styles.overlaytext}>
-            <h1>Explore travel moments worthy of a story to tell.</h1>
-          </div>
-        
-      </div>
+        <div className={styles.overlaytext}>
+          <h1>Explore travel moments worthy of a story to tell.</h1>
+        </div>
+
+      </div><div>
+          <h2>Set up your trip.</h2>
+          <p>Next, where to go?</p>
+        </div>
+        <button class="ripple button">Click Me</button>
+      <section class="container content"></section></>
   );
 }
