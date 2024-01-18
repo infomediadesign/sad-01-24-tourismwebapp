3b55ee05adc702fe0db4fc7e07c8f78cd731b3d5
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-01-18 18:55:05 +0100

    changes

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -1,6 +1,8 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
+
+
 
 export default function Home() {
   return (
@@ -10,8 +12,31 @@ export default function Home() {
       <div className="overlay-text">
         <Navbar />
        
+        <div className={styles.imageContainer}>
+
+
+       
+          <Image
+           src="/Images/Pic.jpeg"
+            alt="Description of the first image"
+            width={1440}
+            height={810}
+          />
+        
+
+
+
+
+
+  <div className={styles.overlayText}>
+    <h1>Explore travel moments worthy of a story to tell.</h1>
+  </div>
+
+
+
+
       </div>
-  
+  </div>
 
 
   )
