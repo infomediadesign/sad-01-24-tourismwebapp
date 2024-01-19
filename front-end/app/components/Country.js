7ec5eb5bbd87d4f7b5d034ea68ef60c0565cbe21
violapgdca <violapgdca@gmail.com>
2024-01-19 20:54:01 +0530

    commit

--- front-end/app/components/Country.js
+++ front-end/app/components/Country.js
@@ -1,18 +1,44 @@
 import React from 'react'
 
+
 export default function Country() {
   return (
+    <div>
+      
+      <img
+        src="/image/malta.jpg"
+        alt="Malta Banner"
+        width="100%"
+        height="auto"/>
+        
     <div>
       <h1>MALTA</h1>
       <p>Malta offers many diverse types of activities to suit the entire family. From walking around Valletta, the walled capital and a UNESCO World Heritage Site, to the famous beaches, boating, and historic sites, there's a great deal to see and do on the islands.</p>
       <div>
         <img
-          src="/image/Malta.jpg"
-          alt="Description of image"
+          src="/image/valleta.webp"
+          alt="valleta"
           width={510}
           height={310} />
       </div>
+      <div>
+        <img
+          src="/image/Gozo.jpg"
+          alt="Gozo"
+          width={510}
+          height={310}
+        />
+      </div>
+      <div>
+        <img
+          src="/image/Mdina-Malta-.jpg"
+          alt="Midna"
+          width={510}
+          height={310}
+        />
+      </div>
       <button>View more attractions</button>
     </div>
+       </div>
   )
 }
