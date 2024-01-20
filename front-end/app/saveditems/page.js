d7afe375fd284584b3f0c4411d498b7b70dc2233
Ankith44 <k.ankithreddy98@aol.com>
2024-01-20 16:45:48 +0530

    UI Screen Proper

--- front-end/app/saveditems/page.js
+++ front-end/app/saveditems/page.js
@@ -1,8 +1,10 @@
-import SavedItems from "../components/saveditems"
+  import SavedItems from "../components/saveditems"
+  import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <div >
+      <Navbar/>
       <SavedItems />
     </div>
   )
