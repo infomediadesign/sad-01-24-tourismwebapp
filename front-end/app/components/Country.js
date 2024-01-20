fb115813fed07ccd41c54e480a9582bed9207efc
violapgdca <violapgdca@gmail.com>
2024-01-20 18:23:29 +0530

    commit

--- front-end/app/components/Country.js
+++ front-end/app/components/Country.js
@@ -1,44 +1,51 @@
-import React from 'react'
-
+import React from 'react';
+import styles from './country.module.css' 
 
 export default function Country() {
   return (
-    <div>
-      
+    <div className={styles.countryContainer}>
       <img
         src="/image/malta.jpg"
         alt="Malta Banner"
-        width="100%"
-        height="auto"/>
-        
-    <div>
-      <h1>MALTA</h1>
-      <p>Malta offers many diverse types of activities to suit the entire family. From walking around Valletta, the walled capital and a UNESCO World Heritage Site, to the famous beaches, boating, and historic sites, there's a great deal to see and do on the islands.</p>
-      <div>
-        <img
-          src="/image/valleta.webp"
-          alt="valleta"
-          width={510}
-          height={310} />
+        className={styles.bannerImage}
+      />
+
+      <div className={styles.contentContainer}>
+        <h1>MALTA</h1>
+        <p>Malta offers many diverse types of activities to suit the entire family. From walking around Valletta, the walled capital and a UNESCO World Heritage Site, to the famous beaches, boating, and historic sites, there's a great deal to see and do on the islands.</p>
+        <div className={styles.imageContainer}>
+
+        <div className={styles.imageWithCaption}>
+          <img
+            src="/image/valleta1.jpg"
+            alt="valleta"
+            className={styles.horizontalImage}
+          />
+           <p className={styles.imageName}>Valletta</p>
+          </div>
+
+          <div className={styles.imageWithCaption}>
+          <img
+            src="/image/Gozo.jpg"
+            alt="Gozo"
+            className={styles.horizontalImage}
+          />
+           <p className={styles.imageName}>Gozo</p>
+           </div>
+
+           <div className={styles.imageWithCaption}>
+          <img
+            src="/image/Mdina-Malta-.jpg"
+            alt="Mdina"
+            className={styles.horizontalImage}
+           />
+           <p className={styles.imageName}>Mdina</p>
+        </div>
+        </div>
+      
+         
+         <button>View more attractions</button>
       </div>
-      <div>
-        <img
-          src="/image/Gozo.jpg"
-          alt="Gozo"
-          width={510}
-          height={310}
-        />
-      </div>
-      <div>
-        <img
-          src="/image/Mdina-Malta-.jpg"
-          alt="Midna"
-          width={510}
-          height={310}
-        />
-      </div>
-      <button>View more attractions</button>
     </div>
-       </div>
-  )
+  );
 }
--- front-end/app/country/page.js
+++ front-end/app/country/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Country from '../components/Country';
-
+import Navbar from '../components/Navbar';
 
 const CountryDetail = () => {
   return (
     <div>
+      <Navbar />
       <Country />
     </div>
   )
