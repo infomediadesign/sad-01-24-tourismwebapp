33d3b1d6a3eebd2f63379b15d02c4f9f647c9961
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-01-21 11:10:45 +0100

    Your commit message

--- country-service/index.js
+++ country-service/index.js
@@ -1,5 +1,6 @@
 const express = require('express'); //creating a express server
 const app = express(); //creating an instance of express app
+<<<<<<< HEAD
 const routes = require('./routes/routes');
 const mongoose = require('mongoose');
 
@@ -18,3 +19,15 @@ mongoose.connect('mongodb+srv://admin:aLDQhMWr2AQ3bsBe@cluster0.yz76vjj.mongodb.
     })
 
 
+=======
+
+const port = "5000"; //port on which app will handle all requests
+
+app.get('/', (req,res) => {
+    res.send('hello world');
+})
+
+app.listen(port, () => {
+    console.log(`App running on ${port}`);
+})
+>>>>>>> 829f9fa (setup index.js file)
