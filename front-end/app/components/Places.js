ba8690cdb4b78eb4df3e58585d81d97d702406b8
violapgdca <violapgdca@gmail.com>
2024-01-21 18:41:02 +0530

    country changes UI complete

---
+++ front-end/app/components/Places.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './places.module.css';
+
+export default function Places() {
+  return (
+    <div className={styles.placesContainer}>
+      <img
+        src="/image/valleta2.jpg"
+        alt="Valletta Banner"
+        className={styles.bannerImage}
+      />
+      <div className={styles.contentContainer}>
+        <div className={styles.headingLeft}>
+          <h1>Valletta</h1>
+          <p className={styles.subHeading}>Malta, Southern Europe</p>
+          </div>
+
+          <button className={styles.saveButton}>
+            save
+          </button>
+          <div className={styles.description}>
+            <p>
+              Malta's capital Valletta is a fortified city located on a hilly
+              peninsula between two of the finest natural harbours in the
+              Mediterranean. The Siege of Malta in 1565 captured the European
+              imagination and mobilised the resources needed to create the new
+              city of Valletta, founded soon after, in 1566. The Knights of St
+              John, aided by the most respected European military engineers of the
+              16th century, conceived and planned the city as a single.
+            </p>
+          </div>
+          <button className={styles.readMoreButton}>Read More</button>
+        </div>
+      </div>
+    
+  );
+}
---
+++ front-end/app/components/country.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styles from './country.module.css' 
+
+export default function Country() {
+  return (
+    <div className={styles.countryContainer}>
+      <img
+        src="/image/malta.jpg"
+        alt="Malta Banner"
+        className={styles.bannerImage}
+      />
+
+      <div className={styles.contentContainer}>
+        <h1>MALTA</h1>
+        <p>Malta offers many diverse types of activities to suit the entire family. From walking around Valletta, the walled capital and a UNESCO World Heritage Site, to the famous beaches, boating, and historic sites, there's a great deal to see and do on the islands.</p>
+        <div className={styles.imageContainer}>
+
+        <div className={styles.imageWithCaption}>
+          <img
+            src="/image/valleta2.jpg"
+            alt="valleta"
+            className={styles.horizontalImage}
+          />
+           <p className={styles.imageName}>Valletta</p>
+          </div>
+
+          <div className={styles.imageWithCaption}>
+          <img
+            src="/image/Gozo.jpg"
+            alt="Gozo"
+            className={styles.horizontalImage}
+          />
+           <p className={styles.imageName}>Gozo</p>
+           </div>
+
+           <div className={styles.imageWithCaption}>
+          <img
+            src="/image/Mdina-Malta-.jpg"
+            alt="Mdina"
+            className={styles.horizontalImage}
+           />
+           <p className={styles.imageName}>Mdina</p>
+        </div>
+        </div>
+      
+         
+         <button>View more attractions</button>
+      </div>
+    </div>
+  );
+}
