08c8207cb506ae059fb821b3ccd25b15cde416bd
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-01-21 20:58:55 +0100

    Your commit message

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -18,17 +18,14 @@
           <h1>Explore travel moments worthy of a story to tell.</h1>
         </div>
 
-      </div><div>
+      </div>
+      <div>
           <h2>Set up your trip.</h2>
           <p>Next, where to go?</p>
-        </div>
+      </div>
+      <button className={styles.button}>See all Destination</button>
+
        <Homepagebody />
-      
-         
-
-
-      
-      
       
       </>
   );
