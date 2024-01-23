5c91309e283c80360e106f7b30b3a5fffd93496d
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-01-23 11:53:54 +0100

    Your commit message

--- front-end/app/page.js
+++ front-end/app/page.js
@@ -5,6 +5,7 @@
   import Navbar from './components/Navbar.js'
   import Homepagebody from './components/Homepagebody';
   import Aboutus from './components/Aboutus';  
+  import Footer from './components/Footer';
 
   export default function Home() {
     return (
@@ -26,6 +27,7 @@
           <p>Next, where to go?</p>
         <Homepagebody />
         <Aboutus />
+        <Footer /> 
         </div>
         </>
   );
