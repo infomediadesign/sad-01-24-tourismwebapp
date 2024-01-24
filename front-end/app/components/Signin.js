e453698ed999402f36ac23818f98a33518f21f37
Pawanputta <pawanputta20@gmail.com>
2024-01-25 01:38:08 +0530

    commit

--- front-end/app/components/Signin.js
+++ front-end/app/components/Signin.js
@@ -2,17 +2,17 @@
 import { useState } from 'react';
 import './App.css';
 
-
-const LoginForm = () => {
+const SigninForm = () => {
   const [captchaValue, setCaptchaValue] = useState('');
 
   const handleRecaptchaChange = (value) => {
     setCaptchaValue(value);
   };
+
   return (
     <div className="container">
       <h1>Welcome</h1>
-      <h2>Login</h2>
+      <h2>Sign In</h2>
       <form action="#">
         <label htmlFor="email">Email Address</label>
         <input type="email" id="email" name="email" required />
@@ -20,10 +20,10 @@ const LoginForm = () => {
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" required />
 
-        <button type="submit">Log in</button>
+        <button type="submit">Sign In</button>
       </form>
     </div>
   );
 };
 
-export default LoginForm;
+export default SigninForm;
---
+++ front-end/app/loginsigin/page.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Signin from '../components/Signin'
+//import Login from './components/Login'
+
+export default function Home() {
+  return (
+    <div>
+     {/* <Login />*/}
+     <Signin />
+    </div>
+  
+  )
+}
