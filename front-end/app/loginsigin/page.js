195c7c39efa2094278bf5c54e47101d4a8c51d9d
Pawanputta <pawanputta20@gmail.com>
2024-01-25 03:43:59 +0530

    commit

--- front-end/app/loginsigin/page.js
+++ front-end/app/loginsigin/page.js
@@ -1,12 +1,15 @@
 import Image from 'next/image'
 import Signin from '../components/Signin'
+import Register from '../components/register'
 //import Login from './components/Login'
 
 export default function Home() {
   return (
     <div>
      {/* <Login />*/}
-     <Signin />
+     {/* <Signin /> */}
+     <Register />
+
     </div>
   
   )
