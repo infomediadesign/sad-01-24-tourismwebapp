a424a31084211aabf28b35fd42bc966913a185b3
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-01-29 18:50:52 +0100

    Saved items page

--- front-end/app/admin/getSavedItems/page.js
+++ front-end/app/admin/getSavedItems/page.js
@@ -6,23 +6,49 @@ import { useRouter } from 'next/navigation';
 import Updatesavedpop from '@/app/components/Updatesavedpop';
 
 export default function page() {
-    const [openPopup, setOpenPopup] = useState(false);
+    
     const [updatePopup, setUpdatePopup] = useState(false);
-    const [countrydetails, setCountryDetail] = useState([]);
-    const [countryId, setCountryId] = useState('');
+    const [saveditemsdetails, setSaveditemsDetail] = useState([]);
+    const [saveditemsId  , setSaveditemsId] = useState('');
     const router = useRouter();
 
+    useEffect(() => {
+      fetch('http://localhost:8000/users/saveditems', { credentials: 'include' })
+        .then(res => res.json())
+        .then(data => {
+          if (data === "Success") {
+            router.push('/admin/getCountry')
+          } else {
+            router.push('/admin')
+          }
+        })
+        .catch((error) => console.error(error));
+    }, [])
+
+
+    useEffect(() => {
+      fetch('http://localhost:8000/saveditems')
+        .then(res => res.json())
+        .then(data => {
+          setSaveditemsDetail(data)
+        })
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+        });
+    }, [])
+  
  const handleUpdate = (id) => () => {
-    // router.push(`/admin/updateCountry/${id}`)
+    
     setUpdatePopup(true);
-    setCountryId(id);
+    setSaveditemsId(id);
   }
 
-  const updatedCountry = () => {
-    fetch('http://localhost:4000/countries')
+
+  const updatedSaveditems = () => {
+    fetch('http://localhost:8000/saveditems')
       .then(res => res.json())
       .then(data => {
-        setCountryDetail(data);
+        setSaveditemsDetail(data);
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
@@ -31,14 +57,14 @@ export default function page() {
 
   const handleDelete = async (id) => {
     try {
-      const response = await fetch(`http://localhost:4000/countries/delete/${id}`, {
+      const response = await fetch(`http://localhost:8000/saveditems/delete/${id}`, {
         method: 'DELETE',
       });
 
       if (response.ok) {
-        console.log(`Country with ID ${id} deleted successfully`);
-        const updatedCountryDetails = countrydetails.filter(detail => detail._id !== id);
-        setCountryDetail(updatedCountryDetails);
+        console.log(`Saveditems with ID ${id} deleted successfully`);
+        const updatedSaveditemsDetails = saveditemsdetails.filter(detail => detail._id !== id);
+        setSaveditemsDetail(updatedSaveditemsDetails);
       }
     } catch (error) {
       console.error('Error during deletion:', error.message);
@@ -66,11 +92,12 @@ export default function page() {
                             </tr>
                         </thead>
                         <tbody>
-                        {countrydetails.map((detail) => (
+                        {saveditemsdetails.map((detail) => (
                             <tr  key={detail._id}>
-                                <td> {detail.name}</td>
-                                <td> {detail.name}</td>
-                                <td> {detail.name}</td>
+                                 <td> {detail.name}</td>
+                                <td> {detail.country}</td>
+                                <td> {detail.place}</td>
+                                <td> {detail.date}</td>
                                 <td className={styles.buttons}>
                                     <button className={styles.add}  onClick={handleUpdate(detail._id)}>Update</button>
                                     <button className={styles.delete}  onClick={(e) => handleDelete(detail._id)}>Delete</button>
@@ -81,8 +108,7 @@ export default function page() {
                     </table>
                 </div>
             </div>
-            {openPopup && <AddCountryPopup setOpenPopup={setOpenPopup} setCountryDetail={setCountryDetail} countryId={countryId} />}
-      {updatePopup && <UpdateCountryPopup setOpenPopup={setUpdatePopup} countryId={countryId} updatedCountryDetails = {updatedCountry} />}
+      {updatePopup && <Updatesavedpop setOpenPopup={setUpdatePopup} saveditemsId={saveditemsId} updatedSaveditemsDetails = {updatedSaveditems} />}
         </div>
     )
 }
