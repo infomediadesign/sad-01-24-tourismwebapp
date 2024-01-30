80434d1960363c249e2d2cf0e8dea6f25bbadc34
MalvinaDsouza <malvina.dsouza21@gmail.com>
2024-01-30 21:30:24 +0100

    redis code

--- saveditems-service/index.js
+++ saveditems-service/index.js
@@ -3,6 +3,12 @@ const app = express(); //creating an instance of express app
 const routes = require('./routes/routes');
 const mongoose = require('mongoose');
 
+import Redis from "ioredis"
+const Redis = require("ioredis");
+const client = new Redis("rediss://default:acda3123066847b389d05569259aa90e@eu2-gorgeous-buzzard-32656.upstash.io:32656");
+await client.set('foo', 'bar');
+
+
 app.use(routes);
 
 const port = "8000"; //port on which app will handle all requests
